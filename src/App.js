b8468e67b04e44a3bce29fe1535b23d661bf34d1
Ron <ronsinnovativevisions@gmail.com>
2024-11-09 16:23:45 -0500

    extension done"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const initialCount = parseInt(localStorage.getItem('count')) || 0;
+  const [count, setCount] = useState(initialCount);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  }
+  const handleSubtract = () => {
+    if (count <= 0) return;
+    setCount(count - 1);
+  }
+  const handleReset = () => {
+    setCount(0);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('count', count);
+  }, [count])
+
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <p>This is a React Chrome Extension</p>
+      <h1 className='text-5xl font-bold text-blue-500 mt-3'>{count}</h1>
+      <div>
+        <button className='btn bg-red-500 hover:bg-red-700' onClick={handleSubtract}>-</button>
+        <button className='ml-2 btn bg-blue-500 hover:bg-blue-700' onClick={handleAdd}>+</button>
+      </div>
+      <button className='btn bg-gray-500 hover:bg-gray-700' onClick={handleReset}>Reset</button>
     </>
   );
 }
